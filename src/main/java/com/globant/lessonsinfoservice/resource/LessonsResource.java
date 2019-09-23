402dd0bb653c0079ffe784d65c42779480ff29b0
Daniel <dvago1988@gmail.com>
2019-09-23 00:45:01 -0500

    Change model names and resource response

--- src/main/java/com/globant/lessonsinfoservice/resource/LessonsResource.java
+++ src/main/java/com/globant/lessonsinfoservice/resource/LessonsResource.java
@@ -1,6 +1,9 @@
 package com.globant.lessonsinfoservice.resource;
 
-import com.globant.lessonsinfoservice.model.LessonInformation;
+import com.globant.lessonsinfoservice.model.Lesson;
+import com.globant.lessonsinfoservice.model.Record;
+import java.time.LocalDate;
+import java.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lessons")
 public class LessonsResource {
 
-    @GetMapping("/numberoflessons/{userId}")
-    public Integer createLesson (@PathVariable("userId") int userId){
-        // Return number of lessons given.
-        return 8;
-
-    }
+  @GetMapping("/numberoflessons/{userId}")
+  public Record getLessons(@PathVariable("userId") int userId) {
+    // Return number of lessons given.
+    return new Record(Arrays.asList(
+        new Lesson(1, userId, "Clase de prueba número 1", LocalDate.now(), "Maria Luisa"),
+        new Lesson(2, userId, "Clase de prueba número 2", LocalDate.now(), "Maria Luisa"),
+        new Lesson(3, userId, "Clase de prueba número 3", LocalDate.now(), "Maria Luisa"),
+        new Lesson(4, userId, "Clase de prueba número 4", LocalDate.now(), "Maria Luisa"),
+        new Lesson(5, userId, "Clase de prueba número 5", LocalDate.now(), "Maria Luisa"),
+        new Lesson(6, userId, "Clase de prueba número 6", LocalDate.now(), "Maria Luisa"),
+        new Lesson(7, userId, "Clase de prueba número 7", LocalDate.now(), "Maria Luisa"),
+        new Lesson(8, userId, "Clase de prueba número 8", LocalDate.now(), "Maria Luisa")
+    ));
+  }
 }
